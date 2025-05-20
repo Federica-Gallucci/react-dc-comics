194c56238d1aacdedd312e630502cb00d3152543
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-05-20 19:16:26 +0200

    add main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,37 @@
 export default function Main() {
   return (
     <main>
-      <div className="container">Main</div>
+      <section className="main-top">
+        <div className="container">
+          <h2>--Content goes here--</h2>
+        </div>
+      </section>
+      <section className="main-content ">
+        <div className="container">
+          <ul>
+            <li>
+              <img src="./buy-comics-digital-comics.png"></img>
+              <div>DIGITAL COMICS</div>
+            </li>
+            <li>
+              <img src="./buy-comics-merchandise.png"></img>
+              <div>DC MERCHANDISE</div>
+            </li>
+            <li>
+              <img src="./buy-comics-subscriptions.png"></img>
+              <div>SUBSCRIPTION</div>
+            </li>
+            <li>
+              <img src="./buy-comics-shop-locator.png"></img>
+              <div>COMICS SHOP LOCATOR</div>
+            </li>
+            <li>
+              <img src="./buy-dc-power-visa.svg"></img>
+              <div>DC POWER VISA</div>
+            </li>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
